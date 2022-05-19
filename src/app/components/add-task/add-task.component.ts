29e3045aaa84635dcd1c5f5440040b2a048b0f85
nguyenhieu409 <hieu.nguyen@successsoftware.global>
2022-05-19 17:33:04 +0700

    f

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTaskComponent implements OnInit {
 
+  text: string = '';
+  day: string = '';
+  reminder: boolean = false;
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onSubmit(){
+    if(!this.text){
+      alert('Please add a task!!!!');
+      return;
+    }
+
+
+    // Những thứ cần gửi đến server
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder
+    }
+
+    // Ko gửi ở nơi đây, mà gửi ở component cha, tạo sự kiện emit event
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
 }
